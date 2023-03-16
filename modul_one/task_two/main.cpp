ee332cb02efcfe896b5e41cf608888ec4ce1e039
Kosmatoff <dimka.komarov@bk.ru>
2023-03-17 00:54:46 +0300

    Создание ветки добавление решение 2 задач

---
+++ modul_one/task_two/main.cpp
@@ -0,0 +1,89 @@
+/*
+2_3. Даны два массива неповторяющихся целых чисел, 
+упорядоченные по возрастанию. 
+A[0..n-1] и B[0..m-1]. n >> m. Найдите их пересечение.
+Требования: Время работы: O(m * log k), где k - позиция элемента B[m-1] в массиве A.. 
+В процессе поиска очередного элемента B[i] в массиве A пользуйтесь результатом поиска элемента B[i-1]. 
+Внимание! В этой задаче для каждого B[i] сначала нужно определить диапазон для бинарного поиска размером 
+порядка k с помощью экспоненциального поиска, а потом уже в нем делать бинарный поиск.
+*/
+
+#include <iostream>
+#include <cassert>
+
+void arrayFiling(int array[], int size) {  // ввод данных в массив
+    for (int i = 0; i < size; i++) {
+        std::cin >> array[i];
+    }
+}
+
+void coutArray(const int array[], int size) {  // вывод данных в консоль
+    for (int i = 0; i < size; i++) {
+        std::cout << array[i] << " ";
+    }
+}
+
+int min(int num1, int num2) {
+    if (num1 > num2) {
+        return num2;
+    }
+    return num1;
+}
+
+int intersectionArrays(
+    const int A[], int sizeA,
+    const int B[], int sizeB,
+    int sect[]
+) {
+    int k = 0;
+    int previous = 0;
+    for (int i = 0; i < sizeB; i++) {
+        if (previous >= sizeA) {
+            break;
+        }
+        // определение диапазона для поиска текущего элемента B
+        int rangeSize = 1;
+        while (previous + rangeSize < sizeA && A[previous + rangeSize] < B[i]) {
+            rangeSize *= 2;
+        }
+        int first = previous;
+        int last = min(previous + rangeSize, sizeA);
+        // бинарный поиск элемента B в диапазоне
+        while (first < last) {
+            int mid = (first + last) / 2;
+            if (A[mid] < B[i]) {
+                first = mid + 1;
+            } else {
+                last = mid;
+            }
+        }
+        if (first < sizeA && A[first] == B[i]) {
+            sect[k] = B[i];
+            k++;
+        }
+        previous = first;  // запоминаем индекс последнего элемента
+    }
+    return k;
+}
+
+
+int main() {
+    int n, m;
+
+    std::cin >> n;
+    std::cin >> m;
+
+    assert(n > 0 && m > 0);
+    const int sizeA = n;
+    const int sizeB = m;
+
+    int A[sizeA];
+    int B[sizeB];
+    arrayFiling(A, sizeA);
+    arrayFiling(B, sizeB);
+
+    int sect[sizeB];
+    int k = intersectionArrays(A, sizeA, B, sizeB, sect);
+    coutArray(sect, k);
+    return 0;
+}
